cafb1ce14cd3443414762131b58d7783cbf14d0c
Vitaly Velikodny <vvelikodny@gmail.com>
2019-06-26 20:44:24 +0300

    frontend#40: Add 'status' field for API errors

--- errors/api_error.go
+++ errors/api_error.go
@@ -3,7 +3,7 @@ package errors
 // APIError represents an error that can be sent in an error response.
 type APIError struct {
 	// Status represents the HTTP status code
-	Status int `json:"-"`
+	Status int `json:"status"`
 	// ErrorCode is the code uniquely identifying an error
 	ErrorCode string `json:"error_code"`
 	// Message is the error message that may be displayed to end users
